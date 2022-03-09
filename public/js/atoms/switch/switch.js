59eb565b5b6cf911a8a9511b7f714b0ed9222e40
Michael Richins <mr.grigri@gmail.com>
2022-03-09 15:21:13 -0700

    Remove: Toggle; if favor of Add: Switch

---
+++ public/js/atoms/switch/switch.js
@@ -0,0 +1,143 @@
+export class SwitchAtom extends HTMLElement {
+  static get RequiredAttributes() {
+    return {
+      Direction: 'switch-direction',
+    };
+  }
+
+  static get SlotNames() {
+    return {
+      Label: 'switch-label',
+      Control: 'switch-control',
+    };
+  }
+
+  #container;
+  #label;
+  #control;
+  #direction;
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+
+    this.#validateAttributes();
+    this.#addStyles();
+    this.#addHtml();
+    this.#setLabel();
+    this.#setSwitch();
+
+    this.#handleControl();
+    this.#addEventListeners();
+    this.#setCheckedState();
+  }
+
+  #validateAttributes() {
+    this.#direction = this.getAttribute(
+      SwitchAtom.RequiredAttributes.Direction
+    );
+
+    if (
+      this.#direction &&
+      this.#direction !== 'rtl' &&
+      this.#direction !== 'ltr'
+    ) {
+      throw new Error(
+        `Valid attributes for ${SwitchAtom.RequiredAttributes.Direction} are either ltr or rtl`
+      );
+    }
+  }
+
+  #addStyles() {
+    const link = document.createElement('link');
+
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', 'css/atoms/switch/switch.css');
+
+    this.shadowRoot.appendChild(link);
+  }
+
+  #addHtml() {
+    const container = this.#generateContainerElement();
+    const labelSlot = this.#generateLabelSlot();
+    const controlSlot = this.#generateControlSlot();
+
+    container.appendChild(labelSlot);
+    container.appendChild(controlSlot);
+
+    this.shadowRoot.appendChild(container);
+    this.#container = container;
+  }
+
+  #generateContainerElement() {
+    const container = document.createElement('div');
+
+    container.classList.add('container');
+    container.classList.add(
+      this.#direction?.length
+        ? `container--${this.#direction}`
+        : `container--ltr`
+    );
+
+    return container;
+  }
+
+  #generateLabelSlot() {
+    const slot = document.createElement('slot');
+
+    slot.name = SwitchAtom.SlotNames.Label;
+
+    return slot;
+  }
+
+  #generateControlSlot() {
+    const slot = document.createElement('slot');
+
+    slot.name = SwitchAtom.SlotNames.Control;
+
+    return slot;
+  }
+
+  #setLabel() {
+    const labelSlot = this.#container.querySelector(
+      `[name="${SwitchAtom.SlotNames.Label}"]`
+    );
+    this.#label = labelSlot
+      .assignedElements()
+      .find((elem) => elem.tagName.toLowerCase() === 'label');
+  }
+
+  #setSwitch() {
+    const controlSlot = this.#container.querySelector(
+      `[name="${SwitchAtom.SlotNames.Control}"]`
+    );
+    this.#control = controlSlot
+      .assignedElements()
+      .find((elem) => elem.tagName.toLowerCase() === 'input');
+  }
+
+  #handleControl() {
+    this.#control.type = 'checkbox';
+    this.#control.setAttribute('role', 'switch');
+    this.#control.setAttribute('aria-checked', 'false');
+  }
+
+  #addEventListeners() {
+    this.#control.addEventListener(
+      'change',
+      this.#handleChangeEvent.bind(this)
+    );
+  }
+
+  #handleChangeEvent(e) {
+    this.#setCheckedState();
+  }
+
+  #setCheckedState() {
+    const isChecked = this.#control.checked;
+
+    this.#control.value = isChecked ? 'on' : 'off';
+    this.#control.setAttribute('aria-checked', `${isChecked}`);
+  }
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
-import { ToggleAtom } from './atoms/toggle/toggle.js';
+import { SwitchAtom } from './atoms/switch/switch.js';
 import { RangeAtom } from './atoms/range/range.js';
 
 const Atoms = new Set([
-  { name: 'pg-toggle', key: ToggleAtom },
+  { name: 'pg-switch', key: SwitchAtom },
   { name: 'pg-range', key: RangeAtom },
 ]);
 
 for (let { name, key } of Atoms) {
+  if (window.customElements.get(name)) break;
   window.customElements.define(name, key);
 }
